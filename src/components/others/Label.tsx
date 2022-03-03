4d91a0d865cc6b3a658b137129c5a700d82d722e
Cyril Lav <cyril.laveyssiere@gmail.com>
2022-03-03 13:38:51 +0100

    updating Label

--- src/components/others/Label.tsx
+++ src/components/others/Label.tsx
@@ -1,6 +1,7 @@
 import { ComponentPropsWithRef, forwardRef } from 'react';
 import { Theme } from '../../common/types';
 import { IconProps } from './Icon';
+import Spacer from './Spacer';
 import { LabelStyle } from './style';
 
 type Children = [IconProps, string] | IconProps | string;
@@ -11,13 +12,22 @@ export interface LabelProps extends ComponentPropsWithRef<'div'> {
 }
 
 const Label = forwardRef<HTMLDivElement, LabelProps>((props, ref) => {
-
 	const { children, theme, className, style } = props;
 
 	const childrenDestructuring = () => {
 		if (children) {
 			if (Array.isArray(children)) {
-				return children.map((component, index) => <>{component}</>);
+				return children.map((component, index) => {
+					if (index !== children.length - 1) {
+						return (
+							<>
+								{typeof component === 'string' ? <span>component</span> : component}
+								<Spacer />
+							</>
+						);
+					}
+					return typeof component === 'string' ? <span>component</span> : component;
+				});
 			}
 			return children;
 		}
