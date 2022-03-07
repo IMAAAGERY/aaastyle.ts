c8e843a01046beb585cb8bb1578b48afa0a9a90b
Cyril Lav <cyril.laveyssiere@gmail.com>
2022-03-07 10:59:46 +0100

    updating dropdown

--- src/components/lists/Dropdown.tsx
+++ src/components/lists/Dropdown.tsx
@@ -2,17 +2,15 @@ import { ComponentPropsWithRef, forwardRef } from 'react';
 import { Theme } from '../../common/types';
 import { DropdownWrapperStyle, DropdownListStyle } from './style';
 
-export interface DropdownProps extends ComponentPropsWithRef<'ul'> {
+export interface DropdownProps extends ComponentPropsWithRef<'div'> {
 	theme?: Theme;
 }
 
-const Dropdown = forwardRef<HTMLUListElement, DropdownProps>((props, ref) => {
+const Dropdown = forwardRef<HTMLDivElement, DropdownProps>((props, ref) => {
 	const { theme, className, style, children } = props;
 	return (
-		<DropdownWrapperStyle>
-			<DropdownListStyle ref={ref} theme={theme} className={className} style={style}>
-				{children}
-			</DropdownListStyle>
+		<DropdownWrapperStyle ref={ref} theme={theme} className={className} style={style}>
+			<DropdownListStyle>{children}</DropdownListStyle>
 		</DropdownWrapperStyle>
 	);
 });
