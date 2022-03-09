d41f61aa990238dcecb0affc55eb2b9ceaf1cf8d
Cyril Lav <cyril.laveyssiere@gmail.com>
2022-03-09 12:28:39 +0100

    fixing select border

--- src/components/inputs/style.ts
+++ src/components/inputs/style.ts
@@ -42,18 +42,17 @@ export const SelectStyle = styled.div<{ rotate?: boolean }>`
 
 export const OptionsWrapperStyle = styled.div<{ show: boolean }>`
 	position: absolute;
-	border: 1px solid rgba(144, 144, 144, 0.3);
+	border: ${(props) => (props.show ? '1px' : 0)} solid rgba(144, 144, 144, 0.3);
 	border-radius: 4px;
 	background-color: #ffffff;
 	font-size: 1em;
 	font-weight: 600;
 	margin-top: 8px;
 	padding: 0;
-	transition: max-height 0.2s ease-in-out;
-	max-height: ${(props) => (props.show ? '100vh' : '0')};
+	transition: all 0.2s ease-in-out;
+	max-height: ${(props) => (props.show ? '100vh' : 0)};
 	overflow: hidden;
 	width: 100%;
-
 `;
 
 export const OptionsListStyle = styled.ul`
